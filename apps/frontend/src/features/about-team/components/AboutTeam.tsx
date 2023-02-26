92d98b249a3b964450eb11ac60b1f425a7e4df20
ka10take <kato.development1618@gmail.com>
2023-02-26 15:30:38 +0900

    :art: ロゴをSVGに差し替え

--- apps/frontend/src/features/about-team/components/AboutTeam.tsx
+++ apps/frontend/src/features/about-team/components/AboutTeam.tsx
@@ -4,7 +4,7 @@ export const AboutTeam = (): JSX.Element => {
   return (
     <div className="flex justify-center items-center max-w-[1000px] m-auto flex-wrap-reverse px-4">
       <div className="flex flex-col items-center md:w-1/2">
-        <img src="../logo.webp" alt="logo" className="w-[100px]" />
+        <img src="../logo.svg" alt="logo" className="w-[100px]" />
 
         <h2 className="font-semibold text-6xl my-10">YCFCとは</h2>
 
