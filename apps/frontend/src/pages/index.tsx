6a5b01ef3e50291ee0e6a1e88162b3792d6090d8
ka10take <kato.development1618@gmail.com>
2023-02-27 22:35:42 +0900

    :recycle: ディレクトリ名変更

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { HeroContent } from "@/features/hero-content/components";
 import { JuniorYouth } from "@/features/junior-youth/components";
 import { News } from "@/features/news/components";
 import { SocialContribution } from "@/features/social-contribution/components";
-import { SponsorRecruiting } from "@/features/sponsor_recruiting/components";
+import { SponsorRecruiting } from "@/features/sponsor-recruiting/components";
 
 export default function Web() {
   return (
