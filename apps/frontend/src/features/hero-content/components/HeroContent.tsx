e114e8d12f8e2b538d0496f2610d989eefc6bf98
ka10take <kato.development1618@gmail.com>
2023-02-27 22:58:23 +0900

    :art: ヒーローのタイトル文字の改行位置指定

--- apps/frontend/src/features/hero-content/components/HeroContent.tsx
+++ apps/frontend/src/features/hero-content/components/HeroContent.tsx
@@ -2,8 +2,9 @@ export const HeroContent = (): JSX.Element => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="absolute z-10 flex flex-col items-center">
-        <p className="p-4 text-2xl text-center text-secondary mb-4 shadow-lg opacity-80 rounded-sm">
-          吉川市のフットボールの文化を創り・育む
+        <p className="p-4 text-2xl text-center text-secondary mb-4 opacity-80 rounded-sm">
+          <span className="inline-block">吉川市のフットボールの</span>
+          <span className="inline-block mt-4">文化を創り・育む</span>
         </p>
       </div>
 
