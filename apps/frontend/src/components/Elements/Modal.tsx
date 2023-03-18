4702716aee24d7606d73bd06705a3d95911db6d4
ka10take <kato.development1618@gmail.com>
2023-03-18 16:15:15 +0900

    :art: モーダルのスタイル修正

--- apps/frontend/src/components/Elements/Modal.tsx
+++ apps/frontend/src/components/Elements/Modal.tsx
@@ -34,7 +34,7 @@ export const Modal = ({ visible, children, onClose }: ModalProps) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 {children}
               </Dialog.Panel>
             </Transition.Child>
