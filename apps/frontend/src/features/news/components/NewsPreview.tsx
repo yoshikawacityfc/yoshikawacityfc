e7099d939a0476a384f4f756793b7e915ee44c4a
ka10take <kato.development1618@gmail.com>
2023-03-19 22:36:03 +0900

    :+1: トップページのもっと見るボタンの機能実装

--- apps/frontend/src/features/news/components/NewsPreview.tsx
+++ apps/frontend/src/features/news/components/NewsPreview.tsx
@@ -1,8 +1,12 @@
 import { Button, ScrollAnimation } from "@/components/Elements";
+import { PagePaths } from "@/lib/pagePaths";
+import { useRouter } from "next/router";
 import { NEWS_DEMO_DATA } from "../data";
 import { NewsCardSliderList } from "./NewsCardSliderList";
 
 export const NewsPreview = (): JSX.Element => {
+  const router = useRouter();
+
   return (
     <ScrollAnimation delay={300} className="flex flex-col items-center">
       <h2 className="font-semibold text-6xl text-center mb-24">お知らせ</h2>
@@ -10,7 +14,11 @@ export const NewsPreview = (): JSX.Element => {
       <NewsCardSliderList news={NEWS_DEMO_DATA} />
 
       <div className="mt-24 max-w-[1000px] w-3/4">
-        <Button label="もっと見る" fullWidth />
+        <Button
+          label="もっと見る"
+          fullWidth
+          onClick={() => router.push(PagePaths.news())}
+        />
       </div>
     </ScrollAnimation>
   );
