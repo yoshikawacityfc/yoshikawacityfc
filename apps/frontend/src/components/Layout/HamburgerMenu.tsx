00847d1d629dbeeca6d7fd80bdeb22767352916c
ka10take <kato.development1618@gmail.com>
2023-03-19 12:22:42 +0900

    :art: ハンバーガーメニューにアイコン追加

--- apps/frontend/src/components/Layout/HamburgerMenu.tsx
+++ apps/frontend/src/components/Layout/HamburgerMenu.tsx
@@ -1,6 +1,8 @@
 import { MENU } from "@/constants";
 import { PagePaths } from "@/lib/pagePaths";
 import Link from "next/link";
+import { Icon } from "../Elements";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 interface HamburgerMenuProps {
   onClose: () => void;
@@ -9,8 +11,8 @@ interface HamburgerMenuProps {
 export const HamburgerMenu = ({ onClose }: HamburgerMenuProps): JSX.Element => {
   return (
     <div className="w-full h-full fixed z-[100] bg-secondary/80 backdrop-blur-lg overscroll-contain p-4">
-      <div className="text-right" onClick={onClose}>
-        閉じる
+      <div className="text-right mb-4">
+        <Icon icon={faXmark} onClick={onClose} size="4xlarge" />
       </div>
 
       <Link href={PagePaths.index()}>
@@ -23,6 +25,7 @@ export const HamburgerMenu = ({ onClose }: HamburgerMenuProps): JSX.Element => {
             return (
               <Link href={item.href} key={index}>
                 <li className="text-primary text-2xl cursor-pointer py-4 border-b-[1px] border-gray-600">
+                  <Icon icon={item.icon} size="xlarge" className="mr-4" />
                   <span className="">{item.label}</span>
                 </li>
               </Link>
