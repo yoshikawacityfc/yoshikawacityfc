c2f7fd78667bb86d3ef97ea48d28d93f0db9ffbc
ka10take <kato.development1618@gmail.com>
2023-04-03 22:43:13 +0900

    :sparkles: お知らせプレビューデータ繋ぎ込み

--- apps/frontend/src/features/news/components/NewsPreview.tsx
+++ apps/frontend/src/features/news/components/NewsPreview.tsx
@@ -1,11 +1,23 @@
 import { Button, ScrollAnimation } from "@/components/Elements";
+import { queryNewsCollection } from "@/lib/gql/news";
 import { PagePaths } from "@/lib/pagePaths";
+import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { NEWS_DEMO_DATA } from "../data";
 import { NewsCardSliderList } from "./NewsCardSliderList";
 
 export const NewsPreview = (): JSX.Element => {
+  const { loading, error, data } = useQuery(queryNewsCollection, {
+    variables: {
+      after: null,
+      first: 10,
+    },
+  });
+
   const router = useRouter();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
 
   return (
     <ScrollAnimation delay={300} className="flex flex-col items-center">
@@ -13,7 +25,9 @@ export const NewsPreview = (): JSX.Element => {
         お知らせ
       </h2>
 
-      <NewsCardSliderList news={NEWS_DEMO_DATA} />
+      {data?.newsCollection?.edges && (
+        <NewsCardSliderList news={data?.newsCollection?.edges} />
+      )}
 
       <div className="mt-24 max-w-[800px] w-3/4">
         <Button
