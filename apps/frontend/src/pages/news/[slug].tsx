99e5ab57c9099112bef019156ecf7126c7f5becd
Ryo Takeuchi <develop@r09i.me>
2023-05-08 20:48:15 +0900

    :+1: ニュースページを ISR に変更

--- apps/frontend/src/pages/news/[slug].tsx
+++ apps/frontend/src/pages/news/[slug].tsx
@@ -1,19 +1,92 @@
 import { MainLayout } from "@/components/Layout";
-import { NextPage } from "next";
-import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { NewsDetail } from "@/features/news/components";
+import client from "@/lib/apolloClient";
+import { queryNews, queryNewsSlugCollection } from "@/lib/gql/news";
+import {
+  QueryNewsQuery,
+  QueryNewsSlugCollectionQuery,
+} from "@/__generated__/graphql";
 
-const News: NextPage = () => {
-  const router = useRouter();
-  const slug = typeof router.query.slug === "string" ? router.query.slug : "";
+interface NewsScreenProps {
+  news: {
+    id: string;
+    slug: string;
+    title: string;
+    publishedAtString: string;
+    content: string;
+  };
+}
 
+const NewsScreen: NextPage<NewsScreenProps> = ({ news }: NewsScreenProps) => {
   return (
     <MainLayout>
       <section className="pt-64 min-h-[70vh]">
-        <NewsDetail slug={slug} />
+        <NewsDetail news={news} />
       </section>
     </MainLayout>
   );
 };
 
-export default News;
+export default NewsScreen;
+
+// noinspection JSUnusedGlobalSymbols
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query<QueryNewsSlugCollectionQuery>({
+    query: queryNewsSlugCollection,
+    variables: {
+      publishedAtFilter: {
+        lte: new Date().toISOString(),
+      },
+    },
+  });
+
+  const paths =
+    data.newsCollection?.edges?.map((edge) => `/news/${edge.node.slug}`) ?? [];
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<NewsScreenProps> = async (
+  context
+) => {
+  const { data } = await client.query<QueryNewsQuery>({
+    query: queryNews,
+    variables: {
+      filter: {
+        slug: {
+          eq: context.params?.slug,
+        },
+        deleted_at: {
+          is: "NULL",
+        },
+        published_at: {
+          lte: new Date().toISOString(),
+        },
+      },
+    },
+  });
+
+  const news = data.newsCollection?.edges?.[0]?.node;
+  if (!news) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      news: {
+        id: String(news.id),
+        slug: news.slug,
+        title: news.title,
+        publishedAtString: news.published_at,
+        content: news.content,
+      },
+    },
+    revalidate: 30,
+  };
+};
