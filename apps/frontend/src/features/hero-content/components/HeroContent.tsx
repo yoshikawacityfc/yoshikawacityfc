d3b70da2257c37e981eefb4b9677e398e1f87307
ka10take <kato.development1618@gmail.com>
2023-05-11 23:11:15 +0900

    :art: ヒーローテキストのスタイル微修正

--- apps/frontend/src/features/hero-content/components/HeroContent.tsx
+++ apps/frontend/src/features/hero-content/components/HeroContent.tsx
@@ -2,7 +2,7 @@ export const HeroContent = (): JSX.Element => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="absolute z-10 flex flex-col items-center">
-        <p className="fadeIn p-4 text-2xl md:text-4xl text-center text-secondary opacity-80 font-light">
+        <p className="fadeIn p-4 text-2xl md:text-4xl text-center text-secondary font-light">
           <span className="inline-block">吉川市の</span>
           <span className="inline-block">フットボール文化を</span>
           <span className="inline-block">創り、育む</span>
