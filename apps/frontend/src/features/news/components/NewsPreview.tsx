f4607b1fafc0da74fea55569e40a804a79407abf
ka10take <kato.development1618@gmail.com>
2023-07-09 20:05:21 +0900

    :bug: 不要なimport定義を削除

--- apps/frontend/src/features/news/components/NewsPreview.tsx
+++ apps/frontend/src/features/news/components/NewsPreview.tsx
@@ -1,11 +1,7 @@
 import { Button, ScrollAnimation } from "@/components/Elements";
-import { queryPreviewNewsCollection } from "@/lib/gql/news";
 import { PagePaths } from "@/lib/pagePaths";
-import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { NEWS_PREVIEW_COUNT } from "../constants";
 import { NewsCardSliderList } from "./NewsCardSliderList";
-import { FilterIs, OrderByDirection } from "@/__generated__/graphql";
 import { NewsListItem } from "../types";
 
 interface NewsPreviewProps {
